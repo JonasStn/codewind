e529a3140dfa9a17cb2fc710fdd8b9248efa16ed
Jonas Stein <jonas.stein@posteo.de>
2023-07-01 14:32:26 +0200

    feat(app-ui): fix app.routes

--- packages/app-ui/src/app/app.routes.ts
+++ packages/app-ui/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: 'ui-articles',
-    loadChildren: () => import('articles-ui').then((m) => m.uiArticlesRoutes),
+    loadChildren: () => import('@articles-ui').then((m) => m.uiArticlesRoutes),
   },
 ];
