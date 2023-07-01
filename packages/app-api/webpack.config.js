a40365633bf6ac760e898496976f3d707b29323c
Jonas Stein <jonas.stein@posteo.de>
2023-07-01 22:31:25 +0200

    chore(project): setup serverless for app api

--- packages/app-api/webpack.config.js
+++ packages/app-api/webpack.config.js
@@ -1,8 +1,21 @@
+const packageJson = require('../../package.json');
 const { composePlugins, withNx } = require('@nx/webpack');
+const GeneratePackageJsonPlugin = require('generate-package-json-webpack-plugin');
+
+const basePackage = {
+  name: 'app-api',
+  version: packageJson.version,
+  main: 'main.js',
+  dependencies: {
+    '@nestjs/platform-express':
+      packageJson.dependencies['@nestjs/platform-express'],
+  },
+};
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.plugins.push(new GeneratePackageJsonPlugin(basePackage));
   return config;
 });
